6dc095ec9c7bfcf60d489b2eb05343e9d0a851af
dgbragas <dgbragas.dev@gmail.com>
2021-02-22 17:49:35 -0300

    feat: configuring application routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from 'styled-components';
 
+import { Routes } from './routes';
+
 import GlobalStyles from './styles/global';
 import initialTheme from './styles/themes/initial';
 
 export default function App(): JSX.Element {
   return (
     <ThemeProvider theme={initialTheme}>
+      <Routes />
       <GlobalStyles />
     </ThemeProvider>
   );

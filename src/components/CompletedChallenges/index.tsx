5a0dbfd46abbcba8b99ec0084ca0f5cf67b9a7e0
dgbragas <dgbragas.dev@gmail.com>
2021-02-24 15:16:41 -0300

    feat: creating <CompletedChallenges />

---
+++ src/components/CompletedChallenges/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles';
+
+export function CompletedChallenges(): JSX.Element {
+  return (
+    <S.Container>
+      <span>Desafios completos</span>
+      <span>5</span>
+    </S.Container>
+  );
+}
---
+++ src/components/CompletedChallenges/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  border-bottom: 1px solid #d7d8da;
+  display: flex;
+  font-weight: 500;
+  justify-content: space-between;
+  margin: 3.5rem 0;
+  padding-bottom: 1rem;
+
+  span:first-child {
+    font-size: 1.25rem;
+  }
+
+  span:last-child {
+    font-size: 1.5rem;
+  }
+`;

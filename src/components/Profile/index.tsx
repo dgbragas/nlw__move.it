0666de6ac5470fae503e0e4912ec15f112d18b4b
dgbragas <dgbragas.dev@gmail.com>
2021-02-24 14:45:18 -0300

    feat: creating <Profile />

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import * as S from './styles';
+
+export function Profile(): JSX.Element {
+  return (
+    <S.Container>
+      <S.Avatar src="https://github.com/diego3g.png" alt="Diego Fernandes" />
+
+      <S.ProfileInfos>
+        <strong>Diego Fernandes</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </S.ProfileInfos>
+    </S.Container>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import { ExperienceBar } from '../components/ExperienceBar';
 import { GridContainer } from '../components/GridContainer';
+import { Profile } from '../components/Profile';
+
+import * as S from './styles';
 
 export default function Home(): JSX.Element {
   return (
     <GridContainer>
       <ExperienceBar currentProgress={50} />
+
+      <S.HomeSection>
+        <S.ProfileContainer>
+          <Profile />
+        </S.ProfileContainer>
+      </S.HomeSection>
     </GridContainer>
   );
 }
